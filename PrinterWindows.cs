6e7c167e9aa3832cd25f99658cebb4c26c326041
odisetadi <oddisetiadi@gmail.com>
2020-05-08 20:30:51 +0700

    polymorhism

---
+++ PrinterWindows.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace polymorphism
+{
+    public class PrinterWindows
+    {
+        public string Namaprinter { get; set; }
+
+        public virtual void Show()
+        {
+            Console.WriteLine();
+        }
+        public virtual void Print()
+        {
+            Console.WriteLine("Canon printer printing......");
+        }
+    }
+}
